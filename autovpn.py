ff9ca55056360f213cb08e2ac8cdb8c2ba755a60
Dennis von Bargen <dennis@vonbargen.se>
2022-08-28 18:32:00 +0200

    Refactored autovpn.py

--- autovpn.py
+++ autovpn.py
@@ -45,63 +45,89 @@ def get_provider(provider_arg, api_key) -> Provider:
     raise ValueError(f"{provider_arg} is not a supported provider")
 
 
+def get_key(args, config):
+    key = args["--key"]
+    if key:
+        return key
+
+    provider_arg = args["PROVIDER"]
+    if not is_provider_defined(provider_arg, config):
+        print(f"{provider_arg} is not a supported provider")
+        sys.exit(1)
+
+    key = config["providers"][provider_arg]["key"]
+    if not key:
+        print("API key must be defined in command or in config file")
+        sys.exit(1)
+
+    return key
+
+
+def up(args, config):
+    key = get_key(args, config)
+    provider_arg = args["PROVIDER"]
+    authorized_keys = config["authorized_keys"]
+
+    try:
+        region = args["REGION"]
+        type_slug = args["--type"] or config["providers"][provider_arg]["type_slug"]
+        provider = get_provider(provider_arg, key)
+
+        print("Creating server...")
+        instance = provider.create_server(region, type_slug, authorized_keys)
+
+        try:
+            agent = Agent()
+
+            print("Installing OpenVPN server...")
+            agent.install_vpn_server()
+
+            print("Connecting to OpenVPN server...")
+            agent.vpn_connect()
+
+        finally:
+            print("Destroying server...")
+            provider.destroy_server(instance)
+
+    except (ProviderError, ValueError) as ex:
+        print(ex)
+        sys.exit(1)
+
+
+def show_regions(args, config):
+    key = get_key(args, config)
+    provider_arg = args["PROVIDER"]
+
+    if not is_provider_defined(provider_arg, config):
+        print(f"{provider_arg} is not a supported provider")
+        sys.exit(1)
+
+    print("Downloading regions...")
+    provider = get_provider(provider_arg, key)
+    regions = provider.get_regions(True)
+    print(*regions, sep="\n")
+
+
+def show_providers(config):
+    print(*config["providers"].keys(), sep="\n")
+
+
 def main():
     with open("./config.yml", "r") as stream:
         config = yaml.safe_load(stream)
 
     args = docopt(__doc__)
-    provider_arg = args["PROVIDER"]
-    authorized_keys = config["authorized_keys"]
-    key = None
-    if provider_arg is not None and is_provider_defined(provider_arg, config):
-        key = args["--key"] or config["providers"][provider_arg]["key"]
 
     if args["up"]:
-        if not is_provider_defined(provider_arg, config):
-            print(f"{provider_arg} is not a supported provider")
-            sys.exit(1)
-
-        if key is None:
-            print("API key must be defined in command or in config file")
-            sys.exit(1)
-
-        try:
-            region = args["REGION"]
-            type_slug = args["--type"] or config["providers"][provider_arg]["type_slug"]
-            provider = get_provider(provider_arg, key)
-
-            print("Creating server...")
-            instance = provider.create_server(region, type_slug, authorized_keys)
-
-            try:
-                agent = Agent()
-
-                print("Installing OpenVPN server...")
-                agent.install_vpn_server()
-
-                print("Connecting to OpenVPN server...")
-                agent.vpn_connect()
-
-            finally:
-                print("Destroying server...")
-                provider.destroy_server(instance)
-
-        except (ProviderError, ValueError) as ex:
-            print(ex)
-            sys.exit(1)
+        up(args, config)
 
     elif args["regions"]:
-        if not is_provider_defined(provider_arg, config):
-            print(f"{provider_arg} is not a supported provider")
-            sys.exit(1)
-
-        print("Downloading regions...")
-        provider = get_provider(provider_arg, key)
-        regions = provider.get_regions(True)
-        print(*regions, sep="\n")
+        show_regions(args, config)
 
     elif args["providers"]:
-        print(*config["providers"].keys(), sep="\n")
+        show_providers(config)
+
+    sys.exit(0)
 
 
 if __name__ == '__main__':
