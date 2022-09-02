6f7a40087430de8e66054f25ef0061aeb6bc093a
Dennis von Bargen <dennis@vonbargen.se>
2022-09-02 21:31:57 +0200

    Fixed stall on exit

--- utils/openvpn.py
+++ utils/openvpn.py
@@ -1,5 +1,6 @@
 import getpass
 import os
+import signal
 import subprocess
 import sys
 
@@ -14,8 +15,10 @@ def wait_for_interrupt(p):
         pass
 
     print("Closing VPN tunnel...")
-    print("This may take a couple of minutes")
-    p.kill()
+    p.terminate()
+    # Only stdout.read works here. Program stalls if server is destroyed before process is terminated.
+    # TODO: Check if this is POSIX-specific
+    p.stdout.read()
 
 
 def connect(config, ovpn_config: str):
@@ -30,12 +33,13 @@ def connect(config, ovpn_config: str):
 
     elif platform.startswith("linux") or platform.startswith("darwin"):
         cmd = f"{command} --config {ovpn_config}"
+
         if os.geteuid() == 0:  # user is root
-            p = subprocess.Popen(cmd, shell=True)
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
             wait_for_interrupt(p)
         else:
             root_password = getpass.getpass(f"Root privileges required, enter password for {os.getlogin()}: ")
-            p = subprocess.Popen(f"sudo -S {cmd}", stdin=subprocess.PIPE, shell=True)
+            p = subprocess.Popen(f"sudo -S {cmd}", stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
             with p.stdin as pipe:
                 pipe.write(root_password.encode("utf8"))
             wait_for_interrupt(p)
